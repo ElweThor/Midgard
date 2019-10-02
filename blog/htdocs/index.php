3a4e6511971df00b8fe01e9c3fe195864ae1e629
ElweThor <elwe.thor@gmail.com>
2019-10-02 09:57:55 +0200

    applied the same www structure to the blog subtree

---
+++ blog/htdocs/index.php
@@ -0,0 +1,12 @@
+<?php
+//include("includes/header.inc");
+echo '<H3 align=center>Midgard: the viking sim</H3>';
+print'<TABLE align=center cellpadding=0 cellspacing=0 width="100%">';
+	print"<TR>
+			<TD valign=top>
+			<img src="../images/Midgard_styl_logo.png" alt="enter">
+		    </TD>
+		</TR>";
+print '</TABLE>';
+//include("includes/footer.inc");
+?>
